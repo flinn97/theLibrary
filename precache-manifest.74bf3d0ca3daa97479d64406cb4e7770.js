dee4af93161a3623fdb7c574c3459ca64f68cba8
flinn97 <taylormdavidson@gmail.com>
2021-12-15 18:00:17 -0700

    first commit

---
+++ precache-manifest.74bf3d0ca3daa97479d64406cb4e7770.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2183b96a992fa80fe1c65d3f9dcfe5a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b81976107f1dead1f722",
+    "url": "/static/css/main.4c61d1c1.chunk.css"
+  },
+  {
+    "revision": "5d7104515070d7be9964",
+    "url": "/static/js/2.89860a60.chunk.js"
+  },
+  {
+    "revision": "9e80ed6ee19e2ff5e581d429458bb084",
+    "url": "/static/js/2.89860a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81976107f1dead1f722",
+    "url": "/static/js/main.54ca4cc1.chunk.js"
+  },
+  {
+    "revision": "fbf771ac11559020264f",
+    "url": "/static/js/runtime-main.352374c8.js"
+  },
+  {
+    "revision": "f607ed81ef8c712eecc9e6f03d8f05d7",
+    "url": "/static/media/library.f607ed81.png"
+  },
+  {
+    "revision": "c375d351f195093e337c04011d6f9468",
+    "url": "/static/media/oldpic.c375d351.jpeg"
+  },
+  {
+    "revision": "73c36d713f0e6a1ed489a176d6251fd8",
+    "url": "/static/media/owl.73c36d71.png"
+  }
+]);
